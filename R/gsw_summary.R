8601767b6af8f8f3799ca372bba34b24556dadb8
Lise Vaudor <lise.vaudor@ens-lyon.fr>
2024-09-26 15:57:50 +0200

    change gsw_summary_plot

--- R/gsw_summary.R
+++ R/gsw_summary.R
@@ -11,7 +11,9 @@ gsw_summary=function(city,conn){
     dplyr::filter(Urban.Aggl==city) %>%
     dplyr::pull(ID)
   result=DBI::dbReadTable(conn,"gsw_summary") %>%
-    dplyr::filter(citycode==thisCityCode)
+    dplyr::filter(citycode==thisCityCode) %>% 
+    dplyr::mutate(city=city) %>% 
+    dplyr::mutate(reach=factor(reach,levels=c("upstream","city","downstream")))
   return(result)
 }
 
@@ -20,7 +22,7 @@ gsw_summary=function(city,conn){
 #' @return a table with gsw_pixels results for the city
 #' @export
 gsw_summary_plot=function(gws_summary_result){
-  result_summary=gws_summary_result %>%
+  result_by_reach_zone=gws_summary_result %>%
     dplyr::group_by(reach,zone) %>%
     dplyr::summarise(propwater=unique(propwater))
   plot=ggplot2::ggplot(gws_summary_result,
@@ -28,15 +30,15 @@ gsw_summary_plot=function(gws_summary_result){
     ggplot2::geom_point(aes(size=n*100,col=as.factor(dir_change)))+
     ggplot2::geom_point(data=result %>% dplyr::filter(main==TRUE),
                         stroke=1,col="black",shape=21, aes(size=n*100))+
-    ggplot2::geom_rect(data=result_summary,
+    ggplot2::geom_rect(data=result_by_reach_zone,
                        ggplot2::aes(x=4,y=1,xmin=4,xmax=4.5, ymin=1,ymax=3),
                        fill="white",stat="identity",color="black")+
-    ggplot2::geom_rect(data=result_summary ,
+    ggplot2::geom_rect(data=result_by_reach_zone ,
                        ggplot2::aes(x=4,y=1,xmin=4,xmax=4.5, ymin=1,ymax=1+2*propwater),
                        fill="lightblue",stat="identity",color="black")+
     ggplot2::scale_size(range = c(0, 30))+
     ggplot2::theme(legend.position="none") +
-    ggplot2::xlab("")+ggplot2::ylab("")+ggplot2::labs(title=city) +
+    ggplot2::xlab("")+ggplot2::ylab("")+ggplot2::labs(title=unique(gws_summary_result$city)) +
     ggplot2::facet_grid(rows=ggplot2::vars(reach),cols=ggplot2::vars(zone))    +
     ggplot2::scale_color_manual(values=c("red","darkgrey","green"))
   return(plot)
